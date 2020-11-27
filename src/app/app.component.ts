76104556a6e7751b0121e36d426e76fe5d4b14a1
Jefferson M Souza <jefferson.msouza86@gmail.com>
2020-11-26 22:39:19 -0300

    making modal form components accessible via screenreaders, with validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalRef } from './shared/components/modal/models/modal-ref';
 import { ModalService } from './shared/components/modal/service/modal.service';
 
@@ -7,7 +8,7 @@ import { ModalService } from './shared/components/modal/service/modal.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('modal')
   modalTemplateRef: TemplateRef<any> | null = null;
 
@@ -15,12 +16,33 @@ export class AppComponent {
 
   modalRef: ModalRef | null = null;
 
-  constructor(private modalService: ModalService) {}
+  form: FormGroup | null = null;
+
+  constructor(
+    private modalService: ModalService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['John', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false],
+    });
+  }
 
   show() {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details',
     });
+  }
+
+  submit(): void {
+    if (this.form?.valid) {
+      console.log(this.form?.value);
+      this.modalRef?.close();
+    }
   }
 }
